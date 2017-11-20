6bbc771a63b644f2646ded6c36a9fb1581a343d1
ggualiato <ggualiato@gmail.com>
2017-11-20 20:13:19 -0200

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectId} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
+  if (error) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID("5a125e7efc3ad388a573ed6c")
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: ObjectId("5a10fb93c670e70d644af423")
+  }, {
+    $set: {
+      name: 'Giovanni'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
