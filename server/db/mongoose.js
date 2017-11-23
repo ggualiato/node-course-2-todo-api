c6036b591799468a24f2b0f6757e4760be57a12c
ggualiato <ggualiato@gmail.com>
2017-11-23 01:27:44 -0200

    details

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<giomongodb>:<giomongodb>@ds117956.mlab.com:17956/mongodb-todo-api' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://giomongodb:giomongodb@ds117956.mlab.com:17956/mongodb-todo-api' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
