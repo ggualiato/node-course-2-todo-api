f33431301e9efabbca9a6aec5740e764764e7927
ggualiato <ggualiato@gmail.com>
2017-11-22 22:55:16 -0200

    Add tests for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,8 +4,12 @@ const request = require('supertest');
 var {app} = require('./../server')
 var {Todo} = require('./../models/todo');
 
+const todos = [{text: 'a'}, {text: 'b'}, {text: 'c'}, {text: 'd'}]
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,7 +28,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text: 'test todo text'}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -43,10 +47,21 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(4);
           done();
         }).catch((e) => done(e));
       });
-  })
+  });
+});
 
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(4);
+    })
+    .end(done);
+  });
 });
