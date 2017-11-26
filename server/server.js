a667842889c6855ff1fae2ae799d620d1beee161
ggualiato <ggualiato@gmail.com>
2017-11-26 00:01:55 -0200

    Add tests for PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.post('/todos', (req, res) => {
     text: req.body.text
   });
 
-  Todo.save().then((doc) => {
+  todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
@@ -94,32 +94,6 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
-
-// app.patch('/todos/:id', (req, res) => {
-//   var id = req.params.id;
-//   var body = _.pick(req.body, ['text', 'completed']);
-//
-//   if (!ObjectID.isValid(id)) {
-//     return res.status(404).send();
-//   }
-//
-//   if (_.isBoolean(body.completed) && body.completed) {
-//     body.completedAt = new Date().getTime();
-//   } else {
-//     body.completed = false;
-//     body.completedAt = null;
-//   }
-//
-//   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
-//     if (!todo) {
-//       return res.status(404).send();
-//     }
-//
-//     res.send({todo});
-//   }).catch((e) => {
-//     res.status(400).send();
-//   })
-// });
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
