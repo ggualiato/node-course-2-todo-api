7b0b79a0f887f0033603cfaef346b0a99e2f1eaf
Giovanni Gualiato <ggualiato@gmail.com>
2018-07-15 01:47:39 -0300

    updating mongodb driver to v3.1.1 and refactoring playground/mongodb-connect

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,22 +1,24 @@
 // const MongoClient = require('mongodb').MongoClient;
-const {MongoClient, ObjectID} = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (error, client) => {
   if (error) {
     return console.log('Unable to connect to MongoDB server');
   }
 
   console.log('Connected to MongoDB server');
 
-  // db.collection('Todos').insertOne({
-  //   text: 'Something to do',
-  //   completed: false
-  // }, (error, result) => {
-  //   if (error) {
-  //     return console.log('Unable to insert Todo', error);
-  //   }
-  //   console.log(JSON.stringify(result.ops, undefined, 2));
-  // });
+  const db = client.db('TodoApp')
+
+  db.collection('Todos').insertOne({
+    text: 'Something to do',
+    completed: false
+  }, (error, result) => {
+    if (error) {
+      return console.log('Unable to insert Todo', error);
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
 
   // db.collection('Users').insertOne({
   //   name: 'Giovanni Gualiato',
@@ -29,5 +31,5 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
   //   console.log(result.ops[0]._id.getTimestamp());
   // });
 
-  db.close();
+  client.close();
 });
